bf3cfffabe56667b1718291fe09b2d6ed2c532c0
Brian Heckel <heckelbri@gmail.com>
2023-12-01 15:17:23 -0700

    Working Local Solution For Day 1

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get
 use serde_json::json;
 
 enum ServerError {
-    Day1NotEnoughParam,
+    Day1NotValidPath,
 }
 
 impl IntoResponse for ServerError {
     fn into_response(self) -> axum::response::Response {
         let (status, error_message) = match self {
-            ServerError::Day1NotEnoughParam => (StatusCode::INTERNAL_SERVER_ERROR, ""),
+            ServerError::Day1NotValidPath => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Not a Valid Path!")
+            }
         };
         let body = Json(json!({ "error" : error_message }));
         (status, body).into_response()
@@ -30,19 +32,27 @@ fn recalibrate(nums: Vec<i64>) -> Result<i64, ServerError> {
     let all_xored = nums
         .into_iter()
         .reduce(|acc, e| acc ^ e)
-        .ok_or(ServerError::Day1NotEnoughParam)?;
+        .ok_or(ServerError::Day1NotValidPath)?;
     Ok(all_xored.pow(3))
 }
 
-async fn recalibrate_ids(Path(ids): Path<Vec<i64>>) -> Result<String, ServerError> {
-    recalibrate(ids).map(|i| i.to_string())
+#[axum::debug_handler]
+async fn recalibrate_ids(Path(path): Path<String>) -> Result<String, ServerError> {
+    let nums: Vec<&str> = path.split('/').collect();
+    let nums: Vec<Result<i64, _>> = nums.into_iter().map(|s| s.parse::<i64>()).collect();
+    if nums.iter().any(|e| e.is_err()) {
+        return Err(ServerError::Day1NotValidPath);
+    }
+    let nums = nums.into_iter().map(|r| r.unwrap()).collect();
+    let sled = recalibrate(nums)?;
+    Ok(sled.to_string())
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/-1/error", get(get_error))
-        .route("/1/:num1/:num2", get(recalibrate_ids))
+        .route("/1/*nums", get(recalibrate_ids))
         .route("/", get(hello_world));
     Ok(router.into())
 }
