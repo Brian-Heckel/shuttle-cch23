a3bc48ce16b2b0bc0c98de2c4933b57962258498
Brian Heckel <heckelbri@gmail.com>
2023-12-08 19:37:32 -0700

    Finished Day 4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Json, Router};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 enum ServerError {
@@ -23,6 +30,67 @@ async fn hello_world() -> &'static str {
 
 async fn get_error() -> impl IntoResponse {
     (StatusCode::INTERNAL_SERVER_ERROR, "Oh No!")
+}
+
+#[derive(Serialize, Deserialize)]
+struct Deer {
+    name: String,
+    strength: u32,
+}
+
+async fn reindeer_cheer(Json(deers): Json<Vec<Deer>>) -> impl IntoResponse {
+    let total = deers
+        .into_iter()
+        .fold(0, |accum, deer| accum + deer.strength);
+    total.to_string()
+}
+
+#[derive(Serialize, Deserialize)]
+struct DeerDetailed {
+    name: String,
+    strength: u32,
+    speed: f32,
+    height: u32,
+    antler_width: u32,
+    snow_magic_power: u32,
+    #[serde(rename = "cAnD13s_3ATeN-yesT3rdAy")]
+    candies_eaten_yesterday: u32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ContestResults {
+    fastest: String,
+    tallest: String,
+    magician: String,
+    consumer: String,
+}
+
+async fn reindeer_contest(Json(deers): Json<Vec<DeerDetailed>>) -> Json<ContestResults> {
+    let fastest = deers
+        .iter()
+        .max_by(|&x, &y| (x.speed).partial_cmp(&y.speed).unwrap())
+        .unwrap()
+        .name
+        .clone();
+    let tallest = deers.iter().max_by_key(|&x| x.height).unwrap().name.clone();
+    let magician = deers
+        .iter()
+        .max_by_key(|&x| x.snow_magic_power)
+        .unwrap()
+        .name
+        .clone();
+    let consumer = deers
+        .iter()
+        .max_by_key(|&x| x.candies_eaten_yesterday)
+        .unwrap()
+        .name
+        .clone();
+    Json(ContestResults {
+        fastest,
+        tallest,
+        magician,
+        consumer,
+    })
 }
 
 fn recalibrate(nums: Vec<i64>) -> Result<i64, ServerError> {
@@ -53,6 +121,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/-1/error", get(get_error))
         .route("/1/*nums", get(recalibrate_ids))
+        .route("/4/strength", post(reindeer_cheer))
+        .route("/4/contest", post(reindeer_contest))
         .route("/", get(hello_world));
     Ok(router.into())
 }
