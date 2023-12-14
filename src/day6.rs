7782e60f66cbe54be6b84208fd3908ffa8ac1fc0
Brian Heckel <heckelbri@gmail.com>
2023-12-13 20:57:57 -0700

    Day 6 Done

--- src/day6.rs
+++ src/day6.rs
@@ -1,15 +1,15 @@
 use axum::{response::IntoResponse, Json};
+use fancy_regex::Regex;
 use serde_json::json;
-use tracing::debug;
 
 #[axum::debug_handler]
 #[tracing::instrument]
 pub async fn count_elves(body: String) -> impl IntoResponse {
+    let re = Regex::new(r"(?=(elf on a shelf))").unwrap();
     let elf_count = body.matches("elf").count();
-    let elf_on_a_shelf = body.matches("elf on a shelf").count();
-    let just_shelf = body.matches("shelf").count() - elf_on_a_shelf;
-    debug!(%elf_count, %elf_on_a_shelf, %just_shelf);
+    let elf_on_shelf = re.find_iter(&body).count();
+    let elf_no_shelf = body.matches("shelf").count() - elf_on_shelf;
     Json(
-        json!( { "elf": elf_count, "elf on a shelf": elf_on_a_shelf, "Shelf with no elf on it": just_shelf  }),
+        json!( { "elf": elf_count, "elf on a shelf": elf_on_shelf, "shelf with no elf on it": elf_no_shelf  }),
     )
 }
