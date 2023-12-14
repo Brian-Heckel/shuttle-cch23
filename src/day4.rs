e6a0da3db83af154cc570acd5e2d2040190da7ad
Brian Heckel <heckelbri@gmail.com>
2023-12-13 21:24:32 -0700

    Finished Day 4 Tasks

--- src/day4.rs
+++ src/day4.rs
@@ -1,6 +1,6 @@
 use axum::{response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
-use tracing::debug;
+use serde_json::json;
 
 use crate::cch_error::ReportError;
 
@@ -25,47 +25,48 @@ pub struct DeerDetailed {
     height: u32,
     antler_width: u32,
     snow_magic_power: u32,
+    favorite_food: String,
     #[serde(rename = "cAnD13s_3ATeN-yesT3rdAy")]
     candies_eaten_yesterday: u32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ContestResults {
-    fastest: String,
-    tallest: String,
-    magician: String,
-    consumer: String,
 }
 
 #[tracing::instrument]
 pub async fn reindeer_contest(
     Json(deers): Json<Vec<DeerDetailed>>,
-) -> Result<Json<ContestResults>, ReportError> {
+) -> Result<Json<serde_json::Value>, ReportError> {
     let fastest = deers
         .iter()
         .max_by(|&x, &y| (x.speed).partial_cmp(&y.speed).unwrap())
-        .unwrap()
-        .name
-        .clone();
-    let tallest = deers.iter().max_by_key(|&x| x.height).unwrap().name.clone();
-    let magician = deers
-        .iter()
-        .max_by_key(|&x| x.snow_magic_power)
-        .unwrap()
-        .name
-        .clone();
+        .unwrap();
+    let tallest = deers.iter().max_by_key(|&x| x.height).unwrap();
+    let magician = deers.iter().max_by_key(|&x| x.snow_magic_power).unwrap();
     let consumer = deers
         .iter()
         .max_by_key(|&x| x.candies_eaten_yesterday)
-        .unwrap()
-        .name
-        .clone();
-    let results = ContestResults {
-        fastest,
-        tallest,
-        magician,
-        consumer,
-    };
-    debug!(?results);
-    Ok(Json(results))
+        .unwrap();
+
+    let fastest_str = format!(
+        "Speeding past the finish line with a strength of {} is {}",
+        fastest.strength, fastest.name
+    );
+    let tallest_str = format!(
+        "{} is standing tall with his {} cm wide antlers",
+        tallest.name, tallest.antler_width
+    );
+    let magician_str = format!(
+        "{} could blast you away with a snow magic power of {}",
+        magician.name, magician.snow_magic_power
+    );
+    let consumer_str = format!(
+        "{} ate lots of candies, but also some {}",
+        consumer.name, consumer.favorite_food
+    );
+    let response_json = json!({
+        "fastest": fastest_str,
+        "tallest": tallest_str,
+        "magician": magician_str,
+        "consumer": consumer_str,
+        }
+    );
+    Ok(Json(response_json))
 }
